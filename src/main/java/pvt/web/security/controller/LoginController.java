1fa6a06d4c83d0d078030470af72fbccfd2d89ad
Shaofei Liu <shaofeiliu@ShaofeideMacBook-Pro.local>
2018-11-22 22:05:16 +0800

    del exception

--- src/main/java/pvt/web/security/controller/LoginController.java
+++ src/main/java/pvt/web/security/controller/LoginController.java
@@ -1,14 +1,14 @@
 package pvt.web.security.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import pvt.web.security.exception.CustomException;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.util.Objects;
 
 @RestController
@@ -18,12 +18,15 @@ public class LoginController {
     @PostMapping("/login")
     public void login(@RequestParam(value = "account") String account,
                       @RequestParam(value = "password") String password,
-                      HttpServletRequest request){
+                      HttpServletRequest request,
+                      HttpServletResponse response) throws IOException {
         if (!Objects.equals("lee", account)) {
-            throw new CustomException(HttpStatus.FORBIDDEN, "account password not match");
+            response.sendRedirect("/login.html");
+            return;
         }
         if (!Objects.equals("123456", password)) {
-            throw new CustomException(HttpStatus.FORBIDDEN, "account password not match");
+            response.sendRedirect("/login.html");
+            return;
         }
         HttpSession session = request.getSession(true);
         session.setAttribute("name", account);
